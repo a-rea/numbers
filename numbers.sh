c0c72b208d4a1b4c4d56244d4b6344f4b08e0ddf
Alejandro Rea <area@chapman.edu>
2020-11-29 22:18:41 -0800

    Adding the numbers script

---
+++ numbers.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# numbers.sh
+# Alejandro Rea
+echo "Enter a number: "
+read -r num
+i=1
+while [ "$i" -le "$num" ]
+do
+	if [ $((i%2)) -eq 0 ]; then
+		echo "$i is odd"
+	else
+		echo "$i is even"
+	fi
+	i=$((i+1))
+done
